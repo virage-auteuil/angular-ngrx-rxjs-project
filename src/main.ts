01553fd0a4dc41a35e9099709afbae07d0d3c474
virage-auteuil <bouchouaric@gmail.com>
2024-11-25 10:45:33 +0100

    ngrx

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { AppComponent } from './app.component';
-import { reducers } from './store'; // Point d'entrée pour tous vos réducteurs
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    StoreModule.forRoot(reducers),
-    StoreDevtoolsModule.instrument({ maxAge: 25 }), // NgRx DevTools
-  ],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
